80d7db97df77af3b2df0179b236e8fa6aa4c3957
Konrad Drewniak <konrad.drewniak@gmail.com>
2023-12-04 18:40:11 +0100

    Remote cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-
+/*
   cloud {
     organization = "organization-name"
 
@@ -7,7 +7,7 @@ terraform {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
